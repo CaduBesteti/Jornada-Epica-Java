8c5578b29e25dd291693e47668221dc17160cab4
CaduBesteti <141333538+CaduBesteti@users.noreply.github.com>
2025-03-07 15:08:55 -0300

    Update MainJornada.java

--- untitled/untitled/src/JogoJornadaEpica/MainJornada.java
+++ untitled/untitled/src/JogoJornadaEpica/MainJornada.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class JogoJornadaEpica {
+public class MainJornada {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
